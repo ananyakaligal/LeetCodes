1836fbc9a7babfa3ca20003488e4458a1c730373
Ananya Sriramareddy Kaligal <126067639+ananyakaligal@users.noreply.github.com>
2024-07-31 23:11:59 +0530

    Time: 1171 ms (94.50%), Space: 106 MB (7.75%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        a=[0]*n
+        ans=0
+        for i in range(2,n):
+            if a[i]:
+                continue
+            ans += 1
+            a[i*i:n:i] = [1] * ((n-1) // i - i + 1)
+        return ans
+        
+        
