5b8d8f55970969e43353305c5c110e272a27cfe1
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2024-08-06 19:58:24 +0530

    Time: 41 ms (27.20%), Space: 16.6 MB (42.98%) - LeetHub

---
+++ 0289-game-of-life/0289-game-of-life.py
@@ -0,0 +1,32 @@
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        rows = len(board)
+        cols = len(board[0])
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+        
+        def valid(r, c):
+            return 0 <= r < rows and 0 <= c < cols
+
+        for r in range(rows):
+            for c in range(cols):
+                liveCount = 0
+                for dr, dc in directions:
+                    newRow, newCol = r + dr, c + dc
+                    if valid(newRow, newCol) and board[newRow][newCol] in {1, 2}:
+                        liveCount += 1
+                if board[r][c] == 1:
+                    if liveCount < 2 or liveCount > 3:
+                        board[r][c] = 2  # 2 means it will die
+                else:
+                    if liveCount == 3:
+                        board[r][c] = -1  # -1 means it will become alive
+
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == 2:
+                    board[r][c] = 0  # Cell dies
+                elif board[r][c] == -1:
+                    board[r][c] = 1  # Cell becomes alive
