f891e0d0e43d20db9d727b58d5f0c2ede2d392e2
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2024-09-17 15:05:08 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (95.78%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if(num==0|| num==1){
+            return true;
+        }
+
+        int start =0;
+        int end=num;
+        int mid=-1;
+
+        while(start<=end){
+            mid=start+(end-start)/2;
+
+            long long square=static_cast<long long>(mid)* mid;
+
+            if (square > num){
+                end=mid-1;
+            }
+            else if(square==num){
+                return true;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return false;
+    }
+};
