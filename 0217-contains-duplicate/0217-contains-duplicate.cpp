0d11f81b03763fb37a7b87e7dfe16d9f86abe9bd
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2024-09-30 10:56:28 +0530

    Time: 84 ms (82.69%), Space: 73.5 MB (50.46%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i = 1; i<nums.size(); i++){
-            if(nums[i]==nums[i-1]){
+        unordered_set<int> seen;
+
+        for(int num:nums){
+            if(seen.count(num)){
                 return true;
             }
+            seen.insert(num);
         }
-        return false;
+    return false;
     }
 };
