56141bf2deb169f33b7f0a52991c2d51bd5037f6
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2024-11-03 19:58:03 +0530

    Time: 91 ms (68.55%), Space: 56.2 MB (36.38%) - LeetHub

---
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        if (k==0){
+            return 0;
+        }else{
+            return (count(nums, k) - count(nums, k-1));
+        }
+    }
+private:
+    int count(vector<int> & nums,int k){
+        int l=0, r=0, sum=0,count=0;
+        unordered_map<int, int> myMap;
+
+        while(r<nums.size()){
+            myMap[nums[r]]++;
+             while(myMap.size()>k){
+                myMap[nums[l]]--;
+
+                if(myMap[nums[l]]==0) myMap.erase(nums[l]);
+                l++;
+             }
+
+             count+=r-l+1;
+             r++;
+        }
+
+    return count;
+    }
+
+};
