81fbd9394cd77cf94b3dd959f5281a7cf5db750e
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2024-11-29 14:50:26 +0530

    Time: 0 ms (100.00%), Space: 17.6 MB (5.18%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,59 +1,35 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        
-  int start = 0;
-  int end = nums.size() - 1;
-  int first = -1;
-  int last = -1;
-  int mid ;
+        // Helper function to find the position of the target (left or right bound)
+        auto findPosition = [&](bool findFirst) {
+            int start = 0, end = nums.size() - 1;
+            int position = -1;
 
-  // find first element 
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
 
-    while(start<=end)
-    {
-        mid = start + (end - start) / 2;
-        if(nums[mid] == target)
-        {
-            first = mid;
-            end = mid -1;
+                if (nums[mid] == target) {
+                    position = mid;
+                    if (findFirst) {
+                        end = mid - 1; // Search in the left half for the first occurrence
+                    } else {
+                        start = mid + 1; // Search in the right half for the last occurrence
+                    }
+                } else if (nums[mid] < target) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
 
-        }
+            return position;
+        };
 
-        else if(nums[mid] < target )
-        start = mid +1;
-        else
-        end = mid -1;
-    }
+        // Find the first and last occurrences
+        int first = findPosition(true);
+        int last = findPosition(false);
 
-
-    // find last 
-
-  start = 0 ; end = nums.size() - 1;
-
-    while(start <= end )
-    {
-       mid = start + (end - start )/2;
-       
-     if(nums[mid] == target)
-     {
-        last = mid;
-        start = mid + 1;
-     }
-
-     else if(nums[mid] < target)
-      start = mid + 1;
-
-     else 
-     end = mid - 1;
-
-    }
-
- vector<int>ans(2);
- ans[0] = first;
- ans[1] = last;
-
- return ans;
-  
+        return {first, last};
     }
 };
