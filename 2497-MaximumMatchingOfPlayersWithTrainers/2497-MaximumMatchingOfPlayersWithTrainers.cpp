786c32631efec477649b2088654b6ffccdf8fc6f
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2025-09-16 00:01:13 +0530

    Create 2497-MaximumMatchingOfPlayersWithTrainers

---
+++ 2497-MaximumMatchingOfPlayersWithTrainers/2497-MaximumMatchingOfPlayersWithTrainers.cpp
@@ -0,0 +1,19 @@
+// Last updated: 9/16/2025, 12:01:13 AM
+class Solution {
+public:
+    int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
+        sort(players.begin(),players.end());
+        sort(trainers.begin(),trainers.end());
+        int i=0,j=0;
+        int count=0;
+
+        while(i<players.size() && j<trainers.size()){
+            if(players[i]<=trainers[j]){
+                i++;
+                count++;
+            }
+            j++;
+        }
+        return count;
+    }
+};
