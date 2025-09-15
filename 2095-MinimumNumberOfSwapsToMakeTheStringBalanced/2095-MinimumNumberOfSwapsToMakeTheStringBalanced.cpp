faf7b645d8b56363073008886aed8840a06bd22c
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2025-09-16 00:01:58 +0530

    Create 2095-MinimumNumberOfSwapsToMakeTheStringBalanced

---
+++ 2095-MinimumNumberOfSwapsToMakeTheStringBalanced/2095-MinimumNumberOfSwapsToMakeTheStringBalanced.cpp
@@ -0,0 +1,16 @@
+// Last updated: 9/16/2025, 12:01:58 AM
+class Solution {
+public:
+    int minSwaps(string s) {
+        int size = 0;
+        int n = s.size();
+        for (int i = 0; i < n; i++) {
+            char ch = s[i];
+            if (ch == '[')
+                size++;
+            else if (size > 0) 
+                size--;
+        }
+        return (size + 1) / 2;
+    }
+};
