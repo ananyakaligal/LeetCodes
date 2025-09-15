700d311e4782cfb91d9c127aacdc938cd72057ef
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2025-09-16 00:01:03 +0530

    Create 2573-RemoveNodesFromLinkedList

---
+++ 2573-RemoveNodesFromLinkedList/2573-RemoveNodesFromLinkedList.cpp
@@ -0,0 +1,46 @@
+// Last updated: 9/16/2025, 12:01:04 AM
+auto speedUP = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNodes(ListNode* head) {
+        stack<ListNode*> stk;
+
+        ListNode* curr = head;
+        while(curr != nullptr){
+            
+            while(!stk.empty() && stk.top()->val < curr->val){
+                stk.pop();
+            }
+
+            stk.push(curr);
+            curr = curr->next;
+        }
+
+        ListNode* temp = nullptr;
+        while(!stk.empty()){
+            curr = stk.top();
+            curr->next = temp;
+            temp = curr;
+            stk.pop();
+        }
+
+        return curr;;
+        
+    }
+};
