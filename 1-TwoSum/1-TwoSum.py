719eab4b32d0f7084f263235c6ed166ccc057049
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2025-09-16 00:11:15 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.py
@@ -0,0 +1,20 @@
+# Last updated: 9/16/2025, 12:11:15 AM
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # Create a dictionary to store the numbers and their indices
+        nums_index={}
+
+        #Iterate over each element
+        for index,num in enumerate(nums):
+            complement = target - num
+
+            if complement in nums_index:
+                return [nums_index[complement],index]
+
+            nums_index[num] = index
+        
