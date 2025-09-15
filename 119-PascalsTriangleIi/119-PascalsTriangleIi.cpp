713a00d6ad1aabf3f5b4e090648da63bd3d03314
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2025-09-16 00:08:54 +0530

    Create 119-PascalsTriangleIi

---
+++ 119-PascalsTriangleIi/119-PascalsTriangleIi.cpp
@@ -0,0 +1,16 @@
+// Last updated: 9/16/2025, 12:08:54 AM
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> prev;
+
+        for(int i=0;i<=rowIndex;i++){
+            vector<int> curr(i+1,1);
+            for(int j=1;j<i;j++){
+            curr[j]=prev[j]+prev[j-1];
+            }
+            prev=curr;
+        }
+        return prev;
+    }
+};
