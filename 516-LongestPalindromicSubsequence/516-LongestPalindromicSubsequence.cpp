eaaf727aeb31650ac23f40b7f581068bc5467fc6
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2025-09-16 00:06:14 +0530

    Create 516-LongestPalindromicSubsequence

---
+++ 516-LongestPalindromicSubsequence/516-LongestPalindromicSubsequence.cpp
@@ -0,0 +1,23 @@
+// Last updated: 9/16/2025, 12:06:14 AM
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        int n = s.size();
+        string s2 = s;
+        reverse(s2.begin(), s2.end());
+
+        vector<int> prev(n + 1, 0), curr(n + 1, 0);
+
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (s[i - 1] == s2[j - 1])
+                    curr[j] = 1 + prev[j - 1];
+                else
+                    curr[j] = max(prev[j], curr[j - 1]);
+            }
+            prev = curr;  // Move current row to previous for next iteration
+        }
+
+        return prev[n];
+    }
+};
