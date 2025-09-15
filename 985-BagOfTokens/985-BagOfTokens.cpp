961a8ac87ac26e71ece407e02b409a47ec238fa4
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2025-09-16 00:04:24 +0530

    Create 985-BagOfTokens

---
+++ 985-BagOfTokens/985-BagOfTokens.cpp
@@ -0,0 +1,29 @@
+// Last updated: 9/16/2025, 12:04:24 AM
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+        sort(tokens.begin(),tokens.end());
+
+        int i=0, j=tokens.size()-1;
+        int maxCount=0;
+        int score=0;
+
+        while(i<=j){
+
+            if(tokens[i]<=power){
+                power-=tokens[i];
+                score++;
+                i++;
+            }
+            else if(score>0){
+                power+=tokens[j];
+                j--;
+                score--;
+            }
+            else break;
+
+            maxCount=max(score, maxCount);
+        }
+        return maxCount;
+    }
+};
