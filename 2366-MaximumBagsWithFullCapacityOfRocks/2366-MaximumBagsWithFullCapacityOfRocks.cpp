14ac0680efd1eced9500edeefd3a511f057f4550
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2025-09-16 00:01:21 +0530

    Create 2366-MaximumBagsWithFullCapacityOfRocks

---
+++ 2366-MaximumBagsWithFullCapacityOfRocks/2366-MaximumBagsWithFullCapacityOfRocks.cpp
@@ -0,0 +1,35 @@
+// Last updated: 9/16/2025, 12:01:21 AM
+class Solution {
+public:
+    int maximumBags(vector<int>& cap, vector<int>& rocks, int additionalRocks) {
+        
+      int n=cap.size();
+        vector<int>diff(n);
+
+        for(int i=0;i<n;i++)
+        {
+            diff[i]=cap[i]-rocks[i];
+
+        }
+
+        sort(diff.begin(),diff.end());
+
+        int ans=0;
+
+        for(int i=0;i<n;i++)
+        {
+            int req=diff[i];
+            if(req<=additionalRocks)
+            {
+                additionalRocks-=req;
+                ans++;
+            }
+            else
+            {
+                break;
+
+            }
+        }
+        return ans;
+    }
+};
