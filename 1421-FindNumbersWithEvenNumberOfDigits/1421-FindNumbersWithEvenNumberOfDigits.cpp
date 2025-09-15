75b615ea6fba3e5850c34eb6d0729d41c0682a1c
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2025-09-16 00:03:21 +0530

    Create 1421-FindNumbersWithEvenNumberOfDigits

---
+++ 1421-FindNumbersWithEvenNumberOfDigits/1421-FindNumbersWithEvenNumberOfDigits.cpp
@@ -0,0 +1,22 @@
+// Last updated: 9/16/2025, 12:03:22 AM
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int n=nums.size();
+        int out=0;
+        
+        for (int i=0; i<n;i++){
+            int x=nums[i];
+            int count=0;
+            while(x!=0){
+                count++;
+                x/=10;
+            }
+            if (count%2==0){
+                out++;
+            }
+            
+        }
+        return out;
+    }
+};
