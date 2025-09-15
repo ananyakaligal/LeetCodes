45073c387f578bb35262de5e1a7e08c6b928b847
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2025-09-16 00:07:30 +0530

    Create 225-ImplementStackUsingQueues

---
+++ 225-ImplementStackUsingQueues/225-ImplementStackUsingQueues.cpp
@@ -0,0 +1,53 @@
+// Last updated: 9/16/2025, 12:07:31 AM
+#include <queue>
+
+class MyStack {
+    std::queue<int> one, two;
+
+public:
+    // Constructor
+    MyStack() {}
+
+    // Push element x onto the stack
+    void push(int x) {
+        one.push(x);
+    }
+
+    // Remove the element on the top of the stack and return it
+    int pop() {
+        int x = one.back();
+        while (!one.empty()) {
+            if (one.front() == one.back()) {
+                one.pop();
+            } else {
+                two.push(one.front());
+                one.pop();
+            }
+        }
+        while (!two.empty()) {
+            one.push(two.front());
+            two.pop();
+        }
+
+        return x;
+    }
+
+    // Get the top element of the stack
+    int top() {
+        return one.back();
+    }
+
+    // Return whether the stack is empty
+    bool empty() {
+        return one.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
