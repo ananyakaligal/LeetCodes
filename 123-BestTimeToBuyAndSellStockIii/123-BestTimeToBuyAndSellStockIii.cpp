076fcba8af311761488fea82d6bf8ca92d1c09f5
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2025-09-16 00:08:48 +0530

    Create 123-BestTimeToBuyAndSellStockIii

---
+++ 123-BestTimeToBuyAndSellStockIii/123-BestTimeToBuyAndSellStockIii.cpp
@@ -0,0 +1,28 @@
+// Last updated: 9/16/2025, 12:08:49 AM
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int m=prices.size();
+        vector<vector<int>> ahead(2, vector<int>(3, 0));
+        vector<vector<int>> curr(2, vector<int>(3, 0));
+
+        //buy=1,sell=0;
+
+        for(int i=m-1;i>=0;i--){
+            for(int buy=0;buy<=1;buy++){
+                for(int cap=1;cap<=2;cap++){
+                int profit=0;
+                if (buy){
+                    profit=max((-prices[i]+ahead[0][cap]),(0+ ahead[1][cap]));
+                }else{
+                    profit=max((prices[i]+ahead[1][cap-1]),(0+ ahead[0][cap]));
+                }
+                curr[buy][cap]=profit;
+            }
+            
+            }
+            ahead=curr;
+        }
+        return curr[1][2];
+    }
+};
