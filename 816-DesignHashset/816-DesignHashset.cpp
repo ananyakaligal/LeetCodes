c65cc6b84b616678624a270a7f23e83de30ebebf
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2025-09-16 00:05:01 +0530

    Create 816-DesignHashset

---
+++ 816-DesignHashset/816-DesignHashset.cpp
@@ -0,0 +1,28 @@
+// Last updated: 9/16/2025, 12:05:01 AM
+class MyHashSet {
+public:
+    vector<bool>v;
+    MyHashSet() {
+        v=vector<bool>(1000001,false);
+    }
+    
+    void add(int key) {
+        v[key]=true;
+    }
+    
+    void remove(int key) {
+        v[key]=false;
+    }
+    
+    bool contains(int key) {
+        return v[key];
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
