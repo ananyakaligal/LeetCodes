1bb32d19bddfa564d697257c3c53063ef08666bd
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2025-09-16 00:01:24 +0530

    Create 2350-FindClosestNumberToZero

---
+++ 2350-FindClosestNumberToZero/2350-FindClosestNumberToZero.py
@@ -0,0 +1,18 @@
+# Last updated: 9/16/2025, 12:01:24 AM
+class Solution:
+    def findClosestNumber(self, nums: List[int]) -> int:
+        diff=nums[0]
+
+        for i in range(0,len(nums)):
+            if abs(nums[i])<=abs(diff):
+                diff=nums[i]
+                if abs(diff) in nums:
+                    diff=abs(nums[i])    
+
+        return diff            
+
+
+
+            
+
+
