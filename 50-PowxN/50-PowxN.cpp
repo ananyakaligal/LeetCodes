040d9fcb0d841de5ecbad9dd9273418c313a2437
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2025-09-16 00:10:10 +0530

    Create 50-PowxN

---
+++ 50-PowxN/50-PowxN.cpp
@@ -0,0 +1,19 @@
+// Last updated: 9/16/2025, 12:10:10 AM
+class Solution {
+public:
+    double myPow(double x, int n) {
+        return binaryExp(x,static_cast<long>(n));
+    }
+private:
+    double binaryExp(double x, long n){
+        if(n==0) return 1;
+ 
+        if(n<0) return (1.0/binaryExp(x,-n));
+
+        if (n % 2 == 1) {
+            return x * binaryExp(x * x, (n - 1) / 2);
+        } else {
+            return binaryExp(x * x, n / 2);
+        }
+    } 
+};
