01e871f722f7ebde3d617728ccf6dc08f7530e73
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2025-09-16 00:05:49 +0530

    Create 617-MergeTwoBinaryTrees

---
+++ 617-MergeTwoBinaryTrees/617-MergeTwoBinaryTrees.cpp
@@ -0,0 +1,29 @@
+// Last updated: 9/16/2025, 12:05:50 AM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        // Base case: if either is null, return the other
+        if (!root1) return root2;
+        if (!root2) return root1;
+
+        // Merge the values
+        root1->val += root2->val;
+
+        // Recursively merge left and right subtrees
+        root1->left = mergeTrees(root1->left, root2->left);
+        root1->right = mergeTrees(root1->right, root2->right);
+
+        return root1;
+    }
+};
