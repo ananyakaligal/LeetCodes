4f2919d916b638ecafcd75d548995aab00fb8f3d
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2025-09-16 00:11:06 +0530

    Create 7-ReverseInteger

---
+++ 7-ReverseInteger/7-ReverseInteger.py
@@ -0,0 +1,18 @@
+# Last updated: 9/16/2025, 12:11:06 AM
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x>=0:
+            SNum=str(x)
+            rev_SNum=int(SNum[::-1])
+        else:
+            SNum=str(abs(x))
+            rev_SNum= -int(SNum[::-1]) 
+
+        if rev_SNum>((2**31)-1) or rev_SNum<(-(2**31)):
+            return 0
+        else:
+            return rev_SNum    
+
+        
+
+        
