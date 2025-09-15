d4b421ab0279e3da743c7a127ca4960fa2f6513f
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2025-09-16 00:01:10 +0530

    Create 2536-DetermineIfTwoEventsHaveConflict

---
+++ 2536-DetermineIfTwoEventsHaveConflict/2536-DetermineIfTwoEventsHaveConflict.cpp
@@ -0,0 +1,11 @@
+// Last updated: 9/16/2025, 12:01:10 AM
+class Solution {
+public:
+    
+    bool haveConflict(vector<string>& event1, vector<string>& event2) {
+        if(event1[1]>=event2[0] && event2[1] >= event1[0]){
+            return true;
+        }
+        return false;
+    }
+};
