4b6fb937cacc04f147895f2f0d75280ea3fffe0b
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2025-09-16 00:10:01 +0530

    Create 58-LengthOfLastWord

---
+++ 58-LengthOfLastWord/58-LengthOfLastWord.cpp
@@ -0,0 +1,18 @@
+// Last updated: 9/16/2025, 12:10:01 AM
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int end = s.length() - 1;
+
+        while (end >= 0 && s[end] == ' ') {
+            end--;
+        }
+
+        int start = end;
+        while (start >= 0 && s[start] != ' ') {
+            start--;
+        }
+
+        return end - start;        
+    }
+};
