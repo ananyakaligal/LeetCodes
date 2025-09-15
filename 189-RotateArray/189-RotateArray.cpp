7f1c1b5a0992d47b0bb1b8e8c2842f1abf25fa62
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2025-09-16 00:08:04 +0530

    Create 189-RotateArray

---
+++ 189-RotateArray/189-RotateArray.cpp
@@ -0,0 +1,20 @@
+// Last updated: 9/16/2025, 12:08:04 AM
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k %= nums.size();
+
+        reverse(nums, 0, nums.size() - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.size() - 1);
+    }
+
+private:
+    void reverse(vector<int>& nums, int left, int right) {
+        while (left < right) {
+            swap(nums[left], nums[right]);
+            left++;
+            right--;
+        }
+    }    
+};
