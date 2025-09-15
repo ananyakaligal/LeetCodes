6e1811c788d425fe322c5f7ac74674ed4c4d54a6
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2025-09-16 00:06:33 +0530

    Create 441-ArrangingCoins

---
+++ 441-ArrangingCoins/441-ArrangingCoins.cpp
@@ -0,0 +1,25 @@
+// Last updated: 9/16/2025, 12:06:33 AM
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int start=1;
+        int end=n;
+        int mid=-1;
+
+        while(start<=end){
+            mid=start+(end-start)/2;
+            long long sum = static_cast<long long>(mid) * (mid + 1) / 2;
+
+            if(sum<n){
+                start=mid+1;
+            }
+            else if(sum==n){
+                return mid;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+    return end;
+    }
+};
