2e24d93b5484184f462162a79379bf7cc112a052
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2025-09-16 00:02:21 +0530

    Create 1894-MergeStringsAlternately

---
+++ 1894-MergeStringsAlternately/1894-MergeStringsAlternately.py
@@ -0,0 +1,13 @@
+# Last updated: 9/16/2025, 12:02:21 AM
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        merge = []
+
+        for x, y in zip(word1, word2):
+            merge.append(x)
+            merge.append(y)
+
+        merge.append(word1[len(word2):])
+        merge.append(word2[len(word1):])
+
+        return "".join(merge)
