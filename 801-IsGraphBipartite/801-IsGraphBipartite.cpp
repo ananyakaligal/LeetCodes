775516244e42c63e2b8557ee6c3bad94722c105d
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2025-09-16 00:05:06 +0530

    Create 801-IsGraphBipartite

---
+++ 801-IsGraphBipartite/801-IsGraphBipartite.cpp
@@ -0,0 +1,31 @@
+// Last updated: 9/16/2025, 12:05:06 AM
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<int> color(n,-1);
+
+        for(int i=0;i<n;i++){
+            if(color[i]==-1){
+                queue<int> q;
+                q.push(i);
+                color[i]=0;
+
+                while(!q.empty()){
+                    int node=q.front();
+                    q.pop();
+
+                    for(int neighbor: graph[node]){
+                        if(color[neighbor]==-1){
+                            color[neighbor]=1-color[node];
+                            q.push(neighbor);
+                        }else if(color[neighbor]==color[node]){
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
