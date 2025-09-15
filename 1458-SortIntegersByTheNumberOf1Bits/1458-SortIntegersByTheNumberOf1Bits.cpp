2b3d6a7bfb671f3013d39f0e3257de37aed1e826
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2025-09-16 00:03:10 +0530

    Create 1458-SortIntegersByTheNumberOf1Bits

---
+++ 1458-SortIntegersByTheNumberOf1Bits/1458-SortIntegersByTheNumberOf1Bits.cpp
@@ -0,0 +1,16 @@
+// Last updated: 9/16/2025, 12:03:10 AM
+class Solution {
+public:
+    static bool compare(int& a, int& b) {
+        int countA = __builtin_popcount(a);
+        int countB = __builtin_popcount(b);
+        if (countA == countB)
+            return a < b; 
+        return countA < countB; 
+    }
+
+    vector<int> sortByBits(vector<int>& arr) {
+        sort(arr.begin(), arr.end(), compare);
+        return arr;
+    }
+};
