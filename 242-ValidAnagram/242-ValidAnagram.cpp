14808f8587a1ff569d7a4894e36fc9fb361d9830
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2025-09-16 00:07:17 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.cpp
@@ -0,0 +1,18 @@
+// Last updated: 9/16/2025, 12:07:17 AM
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+
+        vector<int> count(26, 0);
+
+        for (char c : s) count[c - 'a']++;
+        for (char c : t) count[c - 'a']--;
+
+        for (int i = 0; i < 26; ++i) {
+            if (count[i] != 0) return false;
+        }
+
+        return true;
+    }
+};
