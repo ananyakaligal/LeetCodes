035b29e39d288474f9549db0fde3313c4032162d
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2025-09-16 00:05:29 +0530

    Create 724-FindPivotIndex

---
+++ 724-FindPivotIndex/724-FindPivotIndex.py
@@ -0,0 +1,15 @@
+# Last updated: 9/16/2025, 12:05:29 AM
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        total_sum = sum(nums)
+        prefix_sum = 0
+        
+        for i in range(len(nums)):
+            suffix_sum = total_sum - prefix_sum - nums[i]
+            
+            if prefix_sum == suffix_sum:
+                return i
+            
+            prefix_sum += nums[i]
+            
+        return -1
