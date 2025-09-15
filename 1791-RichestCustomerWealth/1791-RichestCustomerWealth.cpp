1fa3bc6e44ba4b1b6d2d73fec478fa5e635c2b23
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2025-09-16 00:02:38 +0530

    Create 1791-RichestCustomerWealth

---
+++ 1791-RichestCustomerWealth/1791-RichestCustomerWealth.cpp
@@ -0,0 +1,18 @@
+// Last updated: 9/16/2025, 12:02:39 AM
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int x =accounts.size();
+        int y=accounts[0].size();
+        int maximum=0;
+
+        for(int i=0;i<x;i++){
+            int sum=0;
+            for(int j=0;j<y;j++){
+                sum+=accounts[i][j];
+            }
+            maximum=max(sum,maximum);
+        }
+        return maximum;
+    }
+};
