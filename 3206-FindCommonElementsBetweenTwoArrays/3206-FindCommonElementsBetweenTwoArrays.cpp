cb1947246946ec8e0c16f6487803bc86a77c1e25
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2025-09-16 00:00:49 +0530

    Create 3206-FindCommonElementsBetweenTwoArrays

---
+++ 3206-FindCommonElementsBetweenTwoArrays/3206-FindCommonElementsBetweenTwoArrays.cpp
@@ -0,0 +1,19 @@
+// Last updated: 9/16/2025, 12:00:50 AM
+class Solution {
+public:
+    vector<int> findIntersectionValues(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> set1(nums1.begin(), nums1.end());
+        unordered_set<int> set2(nums2.begin(), nums2.end());
+
+        int answer1 = 0, answer2 = 0;
+
+        for (int x : nums1) {
+            if (set2.count(x)) answer1++;
+        }
+        for (int x : nums2) {
+            if (set1.count(x)) answer2++;
+        }
+
+        return {answer1, answer2};
+    }
+};
