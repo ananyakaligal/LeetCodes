00c9ef5cd856a180cb916573d1875dac1b9453e0
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2025-09-17 12:59:11 +0530

    Create 26-RemoveDuplicatesfromSortedArray

---
+++ 26-RemoveDuplicatesfromSortedArray/version/cpp/26-RemoveDuplicatesfromSortedArray_20250917_125911.cpp
@@ -0,0 +1,18 @@
+// Last updated: 9/17/2025, 12:59:11 PM
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()) return 0;
+
+        int i = 0; // index of last unique element
+
+        for (int j = 1; j < nums.size(); j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i + 1; // number of unique elements
+    }
+};
