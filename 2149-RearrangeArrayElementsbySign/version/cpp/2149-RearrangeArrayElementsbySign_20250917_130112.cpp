e686dd94511476d898bbdf06704a7d7fc67c3709
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2025-09-17 13:01:12 +0530

    Create 2149-RearrangeArrayElementsbySign

---
+++ 2149-RearrangeArrayElementsbySign/version/cpp/2149-RearrangeArrayElementsbySign_20250917_130112.cpp
@@ -0,0 +1,20 @@
+// Last updated: 9/17/2025, 1:01:12 PM
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> ans(nums.size());
+        int posIndex = 0;
+        int negIndex = 1;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] >= 0) {
+                ans[posIndex] = nums[i];
+                posIndex += 2;
+            } else {
+                ans[negIndex] = nums[i];
+                negIndex += 2;
+            }
+        }
+        return ans;
+    }
+};
