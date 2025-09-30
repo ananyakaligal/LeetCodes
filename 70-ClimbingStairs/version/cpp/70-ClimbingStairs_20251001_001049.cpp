887644255749dbaa8b167675f8dc52bb5f37f22e
Ananya Sriramareddy Kaligal <ananyask.btech22@rvu.edu.in>
2025-10-01 00:10:44 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/version/cpp/70-ClimbingStairs_20251001_001049.cpp
@@ -0,0 +1,15 @@
+// Last updated: 10/1/2025, 12:10:49 AM
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 0 || n == 1) return 1; 
+        int prev2=1, prev1=1;
+        int curr=0;
+        for(int i=2;i<=n;i++){
+            curr=prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
+        }
+        return curr;
+    }
+};
